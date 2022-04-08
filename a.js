2593a2a770f13ba1018823192466c0aeec6ead56
Joseph <495744643@qq.com>
2022-04-08 16:47:13 +0800

    fix: dev

--- a.js
+++ a.js
@@ -1,11 +1,11 @@
 /*
  * @Author: Joseph
  * @Date: 2022-04-08 16:42:37
- * @LastEditTime: 2022-04-08 16:44:36
+ * @LastEditTime: 2022-04-08 16:47:02
  * @Description: 
  * @FilePath: /test/a.js
  */
 function sayHello() {
-  const name = 'joseph111';
+  const name = 'joseph';
   return `hello, ${name}`;
 }
