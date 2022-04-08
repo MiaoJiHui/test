8dbc955e9956f005f7525696e99e380815b937f8
Joseph <495744643@qq.com>
2022-04-08 16:53:33 +0800

    fix: 改了b.js，a.js文件不变

--- b.js
+++ b.js
@@ -1,10 +1,10 @@
 /*
  * @Author: Joseph
  * @Date: 2022-04-08 16:43:27
- * @LastEditTime: 2022-04-08 16:43:37
+ * @LastEditTime: 2022-04-08 16:53:19
  * @Description: 
  * @FilePath: /test/b.js
  */
 function helloworld() {
-  return 'helloworld'
+  return 'helloworld111'
 }
