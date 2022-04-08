de5992ca3866a5dca944521a3650540d57e9d0d9
Joseph <495744643@qq.com>
2022-04-08 16:45:01 +0800

    fix: 修改了a.js中name的名字，改成joseph111

---
+++ a.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Joseph
+ * @Date: 2022-04-08 16:42:37
+ * @LastEditTime: 2022-04-08 16:44:36
+ * @Description: 
+ * @FilePath: /test/a.js
+ */
+function sayHello() {
+  const name = 'joseph111';
+  return `hello, ${name}`;
+}
---
+++ b.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Joseph
+ * @Date: 2022-04-08 16:43:27
+ * @LastEditTime: 2022-04-08 16:43:37
+ * @Description: 
+ * @FilePath: /test/b.js
+ */
+function helloworld() {
+  return 'helloworld'
+}
